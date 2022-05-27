791afdd8d805bc050b2cefeb7820e85ba80956f8
manoel <jmsneto120@gmail.com>
2022-05-27 07:09:22 -0400

    update icon total expense

--- src/components/TotalExpense.tsx
+++ src/components/TotalExpense.tsx
@@ -1,4 +1,4 @@
-import { BookmarkIcon } from "@heroicons/react/solid";
+import { ChartPieIcon } from "@heroicons/react/solid";
 import { useWallet } from "../hooks/useWallet";
 import { formatPrice } from "../utils/formatPrice";
 
@@ -14,7 +14,7 @@ export const TotalExpense = () => {
   return (
     <div className="text-lg sm:text-2xl flex flex-col items-center justify-center text-zinc-700 bg-white w-full md:w-fit h-fit mb-3 py-2 px-4 rounded-xl shadow-md">
       <span className="flex items-center justify-center gap-x-2">
-        <BookmarkIcon className="h-4" />
+        <ChartPieIcon className="h-4" />
         Total de Despesas:
       </span>
       <span className=''>{total}</span>
